da6d395c310e4168c9b5690267486be50a5a991e
Taylor Blocker <taylorblocker@gmail.com>
2019-12-21 12:14:06 -0600

    cleaned up db and got migrations working. Need to adjust and link tables as needed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const APP = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+const knex = require('./db/knex/knex')
 
 console.log(process.env.SECRET_MESSAGE)
 
 APP.get('/', (req, res) => res.send('Hello World!'))
 
-APP.listen(port, () => console.log(`Example app listening on port ${PORT}!`))
+APP.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
