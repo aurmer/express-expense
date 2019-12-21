8a353f6148924a773e7a030ece024950c42ea876
Taylor Blocker <taylorblocker@gmail.com>
2019-12-21 09:57:32 -0600

    worked on getting the server to start. I was having issues with my globally installed ESLINT causing problems

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const APP = express()
 const PORT = 3000
 
+console.log(process.env.SECRET_MESSAGE)
+
 APP.get('/', (req, res) => res.send('Hello World!'))
 
 APP.listen(port, () => console.log(`Example app listening on port ${PORT}!`))
