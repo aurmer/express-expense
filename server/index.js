99935f63860f74eb9db1274699997ca343b6ccd5
Taylor Blocker <taylorblocker@gmail.com>
2019-12-30 19:02:53 -0600

    begging to lay out OAuth with passport.js and google oauth

--- server/index.js
+++ server/index.js
@@ -2,6 +2,43 @@ const express = require('express');
 const APP = express();
 const PORT = process.env.PORT || 3000;
 const { db } = require('./db/dbConnection');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+const {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET} = require('./googleCredentials')
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET,
+			callbackURL: 'http://www.example.com/auth/google/callback',
+		},
+		function(accessToken, refreshToken, profile, done) {
+			User.findOrCreate({ googleId: profile.id }, function(err, user) {
+				return done(err, user);
+			});
+		}
+	)
+);
+
+// Authentication //
+
+APP.use(passport.initialize());
+APP.use(passport.session());
+
+APP.get('/success', (req, res) => res.send('you have successfully logged in'));
+APP.get('/error', (req, res) => res.send('error logging in'));
+
+passport.serializeUser((user, cb) => {
+	cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+	cb(null, obj);
+});
+
+// Test DB Connections //
 
 function testUsersCall() {
 	const userTableQuery = `
