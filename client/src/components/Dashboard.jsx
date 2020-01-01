a010a1cdc1f820dfbb1f0d7603f18fa6dd15fab0
Joey Killam <jhkillam@gmail.com>
2020-01-01 12:06:03 -0600

    removed old placeholder server data

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -10,63 +10,6 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import ReceiptImage from '../img/receipt.svg'
 // import moreImage from '../img/more.svg'
 
-// const fakeExpenseItemsTable = [
-//   {
-//     receipt_name: 'Bahn Mi for Flex Class',
-//     transaction_detail: 'Purchased Bahn Mi for Saturday Class.',
-//     amount: 200.0,
-//     expense_date: '12/21/19',
-//     tags: 'Food',
-//     status: 'Not submitted',
-//     bucket_id: 1
-//   }, {
-//     receipt_name: 'Energy Drinks',
-//     amount: 79,
-//     expense_date: '10/19/19',
-//     tags: 'Food',
-//     status: 'Paid',
-//     bucket_id: 1
-//   }, {
-//     receipt_name: 'Lunch with client',
-//     amount: 34,
-//     expense_date: '11/23/19',
-//     tags: 'Food',
-//     status: 'Pending',
-//     bucket_id: 2
-//   }, {
-//     receipt_name: 'Tacos for demo day',
-//     amount: 121,
-//     expense_date: '12/13/19',
-//     tags: 'Food',
-//     status: 'Pending',
-//     bucket_id: 1
-//   }, {
-//     receipt_name: 'Coffee for team',
-//     amount: 17,
-//     expense_date: '12/18/19',
-//     tags: 'Food',
-//     status: 'Not submitted',
-//     bucket_id: 2
-//   }
-// ]
-// const fakeUserTable = {
-//     first_name: 'John',
-//     last_name: 'Doe',
-//     email: 'John.Doe@Gmail.com',
-//     token: 'test',
-//   }
-// const fakeBucketsTable = [
-//   {
-//     id: 1,
-//     bucket_name: 'DigitalCrafts',
-//     total_number_of_expenses: 3,
-//   }, {
-//     id: 2,
-//     bucket_name: 'Luminare', 
-//     total_number_of_expenses: 2
-//   }
-// ]
-
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
@@ -76,43 +19,6 @@ class Dashboard extends React.Component {
         expenses: []
     };
   }
-  // THIS FUNCTION TO BE REPLACED WITH CALL TO DB
-  // generateFakeTableRows() {
-  //   let fakeTableRows = []
-  //   for (let index = 0; index < 10; index++) {
-  //     // console.log(index)
-  //     fakeTableRows.push({
-  //       category: fakeBucketsTable[0].bucket_name,
-  //       description: fakeExpenseItemsTable[0].receipt_name,
-  //       amount: fakeExpenseItemsTable[0].amount,
-  //       date: fakeExpenseItemsTable[0].expense_date,
-  //       status: fakeExpenseItemsTable[0].status
-  //     })
-  //   }
-  //   return (
-  //     <tbody>
-  //       {fakeExpenseItemsTable.map((expense, index) => (
-  //         <tr key={index}>
-  //           <td>
-  //             {expense.category}
-  //           </td>
-  //           <td>
-  //             {expense.description}
-  //           </td>
-  //           <td>
-  //             {expense.amount}
-  //             <a href="#">
-  //               <img className="receipt-image" alt="receipt image" src={ReceiptImage}></img>
-  //             </a>
-  //           </td>
-  //           <td>
-  //             {expense.date}
-  //           </td>
-  //         </tr>
-  //       ))}
-  //     </tbody>
-  //   )
-  // }
   fetchUserName() {
     fetch(process.env.REACT_APP_API_SERVER + "/get-user")
       .then(response => response.json())
@@ -158,7 +64,6 @@ class Dashboard extends React.Component {
     this.fetchExpenses()
   }
   render() {
-    // this.fetchExpenses()
     return (
       <div className="component-container">
         <h1 style={{height: "48px"}}>{this.state.greeting}</h1>
