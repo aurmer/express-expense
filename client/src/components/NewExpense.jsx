299cfa74ece0fb4192da3a884e9d7887c27e584a
Aubrey <aubrey.snider@gmail.com>
2020-01-07 10:17:45 -0600

    WIP modifying 'new category' behavior

--- client/src/components/NewExpense.jsx
+++ client/src/components/NewExpense.jsx
@@ -56,11 +56,25 @@ class NewExpenseForm extends React.Component {
       }
     }))
   }
+
+  shouldUpdateCatagories(data) {
+    console.log("~~data~~\n",data)
+      console.log("~~catagories~~\n",this.state.catagories)
+    if(data === this.state.catagories) {
+      return false
+    } else {
+      return true
+    }
+  }
+
   fetchCategories() {
     fetch("/get-categories")
     .then(response => response.json())
     .then(data => {
-        this.setState({ categories: data})
+        if(this.shouldUpdateCatagories(data))
+        {
+          this.setState({ categories: data})
+        }
     })
   }
   renderCategories(categories) {
@@ -75,23 +89,16 @@ class NewExpenseForm extends React.Component {
       </>
     )
   }
+
   componentDidMount() {
     this.fetchCategories()
   }
+
+  componentDidUpdate() {
+    this.fetchCategories()
+  }
+
   render() {
-    // MAYBE REMOVE THIS? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // const receipt_image_box = (
-    //   <>
-    //     <div className="box__input">
-    //       <input className="box__file" type="file" name="receipt" id="file" accept="image/*" />
-    //       <label htmlFor="file"><strong>Choose a file</strong><span className="box__dragndrop"> or drag it here</span>.</label>
-    //       <button className="box__button" type="submit">Upload</button>
-    //     </div>
-    //     <div className="box__uploading">Uploading&hellip;</div>
-    //     <div className="box__success">Done!</div>
-    //     <div className="box__error">Error! <span></span>.</div>
-    //   </>
-    // )
 
     return (
       <div>
